22eeedd1b33a96bdc69f201fec15171ee42a5e1e
root <richkibz@gmail.com>
2022-09-09 20:25:14 +0300

    fixes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,26 +8,34 @@
  */
 int main(void)
 {
-	int i, j;
+	int i, e, g, h, q, t;
 
-	for (j = 0 ; j < 99 ; j++)
+	for (h = 48 ; h < 58 ; h++)
 	{
-		for (i = j ; i <= 99 ; i++)
+		for (g = 48 ; g < 58 ; g++)
 		{
-			if (i != j)
+			for (e = 48 ; e < 58 ; e++)
 			{
-			putchar(j / 10 + 48);
-			putchar(j % 10 + 48);
-			putchar(' ');
-			putchar(i / 10 + 48);
-			putchar(i % 10 + 48);
-			}
-			if (j * 100 + i != 9899)
-			{
-				putchar(',');
-				putchar(' ');
+				for (i = 48 ; i < 58 ; i++)
+				{
+					q = (h * 10) + g;
+					t = (e * 10) + i;
+					if (q < t)
+					{
+					putchar(h);
+					putchar(g);
+					putchar(' ');
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
 		}
+
 	}
 	putchar('\n');
 	return (0);
