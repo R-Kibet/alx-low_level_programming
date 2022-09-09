0290e339b8166885ba70623ae4d75c8a8373ffd5
R-Kibet <richkibz@gmail.com>
2022-09-09 11:06:32 -0700

    advanced fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,21 +10,24 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 48 ; i < 58 ; i++)
+	for (i = 48 ; i < 57 ; i++)
 	{
-		for (j = 48 ; j < 58 ; j++)
+		for (j = 49 ; j < 58 ; j++)
 		{
+			if (i != j && i < j)
+			{
 			putchar(i);
 			putchar(j);
-		if (i == 57 && j == 57)
-		{
-			break;
-		}
-		else
-		{
+			if (i == 57 && j == 56)
+			{
+				break;
+			}
+			else
+			{
 			putchar(',');
 			putchar(' ');
-		}
+			}
+			}
 		}
 	}
 	putchar('\n');
