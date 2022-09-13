36c4d6469215e181e841eeda982fd439bb21f723
root <richkibz@gmail.com>
2022-09-13 21:00:13 +0300

    final

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,26 +8,41 @@
 
 int main(void)
 {
-	long i, j, k, l, n;
+	int i;
+	unsigned long j, k, l;
+	unsigned long a, b, c, e;
 
-	n = 91;
-	j = 1;
+	i = 0;
+	j = 0;
 	k = 1;
-
-	for (i = 1 ; i <= n ; i++)
+	for (i = 1; i <= 91; i++)
 	{
-		if (i == 98)
-		{
-			printf("%lu, ", k);
-		}
-		else
-		{
-			printf("%lu, ", k);
-			l = j + k;
-			j = k;
-			k = l;
-		}
+		l = j + k;
+		j = k;
+		k = l;
+		printf("%lu, ", l);
 	}
-	printf("\n");
+	a = j % 1000;
+	j = j / 1000;
+	b = k % 1000;
+	k = k / 1000;
+	while (i <= 98)
+	{
+		e = (a + b) / 1000;
+		c = (a + b) - e * 1000;
+		l = (j + k) + e;
+		a = b;
+		b = c;
+		j = k;
+		k = l;
+		if (c >= 100)
+			printf("%lu%lu", l, c);
+		else
+			printf("%lu0%lu", l, c);
+		if (i != 98)
+			printf(", ");
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
