61fdb2b62375de5cd6c37281a830b437fbfdd112
root <richkibz@gmail.com>
2022-10-27 20:37:58 +0300

    insert node at a certain position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,11 +15,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 
-	if (head == NULL)
+	if (head == NULL && idx)
 		return (NULL);
 	if (idx)
 	{
-		while (i < idx - 1)
+		while (i < idx - 1 && temp)
 		{
 			temp = temp->next;
 			i++;
