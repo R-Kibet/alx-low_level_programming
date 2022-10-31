ce185720706fececc9ece8a1c1333b1eeac3d448
root <richkibz@gmail.com>
2022-10-31 18:12:37 +0300

    fliping bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * flip_bits - function to flip bits
+ * @n: first in
+ * @m: masked int
+ * Return: flip bits
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i;
+
+	for (i = 0 ; n || m; n >>= 1, m >>= 1)
+	{
+		if ((n & 1) != (m & 1))
+			i++;
+	}
+	return (i);
+}
