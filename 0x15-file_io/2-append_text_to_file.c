47fe2df28a3488c5580f2e703dad26bdc6b7602a
root <richkibz@gmail.com>
2022-11-02 21:25:25 +0300

    hot fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,7 +9,7 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fl, w, i;
+	int fl, i, w;
 
 	if (!filename)
 		return (-1);
@@ -19,16 +19,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fl == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content)
 	{
-		;
-	}
-	for (i = 0 ; text_content[i]; i++)
+		for (i = 0; text_content[i]; i++)
 			continue;
-	w = write(fl, text_content, i);
 
-	if (w == -1)
-		return (-1);
+		w = write(fd, text_content, i);
+
+		if (w == -1)
+			return (-1);
+	}
+
 	close(fl);
+
 	return (1);
 }
