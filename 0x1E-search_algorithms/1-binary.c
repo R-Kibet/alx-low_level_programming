084c08f41864f3ede535ff0397705256f21db087
root <richkibz@gmail.com>
2023-04-12 08:46:57 +0300

    binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,50 @@
+#include "search_algos.h"
+
+/**
+ * binary_recursive - recusrive algorithm for binary search
+ * @array: pointer to first position
+ * @size: length of array
+ * @value: element to search
+ * Return: index of value
+ */
+size_t binary_recursive(int *array, size_t size, int value)
+{
+	size_t mid = size / 2;
+	size_t aux;
+
+	if (!array || size == 0)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (aux = 0; aux < size; aux++)
+		printf("%d%s", array[aux], (aux != size - 1) ? ", " : "\n");
+
+	if (mid && size % 2 == 0)
+		mid--;
+	if (value == array[mid])
+		return (mid);
+	if (value < array[mid])
+		return (binary_recursive(array, mid, value));
+
+	mid++;
+	return (binary_recursive(array + mid, size - mid, value) + mid);
+}
+
+/**
+ * binary_search - using binary search must be sorted
+ * @array: pointer to first ellement of array
+ * @size: numner of elements
+ * @value: value to be found
+ * Return: value
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int el;
+
+	el = binary_recursive(array, size, value);
+
+	if (el >= 0 && array[el] != value)
+		return (-1);
+	return (el);
+}
